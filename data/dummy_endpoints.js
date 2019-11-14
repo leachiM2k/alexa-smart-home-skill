48087750dc03563106f97fffe1efd32c8a90f5a6
Michael Rotmanov <leachiM2k@leachiM2k.de>
2019-11-14 14:19:48 +0100

    adjusted publish script

--- data/dummy_endpoints.js
+++ data/dummy_endpoints.js
@@ -28,7 +28,18 @@ const buildEndpoint = (id, manufacturerName, friendlyName, description) => ({
 });
 
 module.exports = [
+/*
+      buildEndpoint('2', 'leachiM2k Raspberry', 'Schrank', '001'),
+      buildEndpoint('3', 'leachiM2k Raspberry', 'Fenster', '002'),
+      buildEndpoint('4', 'leachiM2k Raspberry', 'Sessel', '003'),
+      buildEndpoint('10', 'leachiM2k Raspberry', 'Schlafzimmer', '010'),
+      buildEndpoint('20', 'leachiM2k Raspberry', 'Kinderzimmer', '020'),
+      buildEndpoint('30', 'leachiM2k Raspberry', 'Küche', '030'),
+*/
       buildEndpoint('0', 'leachiM2k Raspberry', 'Schrank', '000'),
       buildEndpoint('1', 'leachiM2k Raspberry', 'Fenster', '001'),
       buildEndpoint('2', 'leachiM2k Raspberry', 'Sessel', '002'),
+      buildEndpoint('3', 'leachiM2k Raspberry', 'Schlafzimmer', '003'),
+      buildEndpoint('4', 'leachiM2k Raspberry', 'Kinderzimmer', '004'),
+      buildEndpoint('5', 'leachiM2k Raspberry', 'Küche', '005'),
 ];
--- publish.sh
+++ publish.sh
@@ -4,11 +4,12 @@ ZIP_FILE=$LAMBDA_NAME-dist.zip
 
 rm -rf dist
 mkdir dist
-yarn install --prod --modules-folder dist/node_modules
-cp -r *.js data dist/
-
-rm -f $ZIP_FILE
 cd dist
+cp -r ../package.json ../package-lock.json ../*.js ../data .
+yarn install --prod
+# or: npm ci
+
+rm -f ../$ZIP_FILE
 zip -X -r ../$ZIP_FILE .
 cd -
 aws lambda update-function-code --profile rotmanov@gmx.de --function-name $LAMBDA_NAME --zip-file fileb://$ZIP_FILE
